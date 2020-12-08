699642f1a3b154f98156cae0ae562b315641f1e2
Kiwon Hong <rusdlsla@gmail.com>
2020-12-08 20:04:56 +0900

    react 기초 5강 : state 변경 하는 법

--- src/App.js
+++ src/App.js
@@ -10,17 +10,22 @@ function App() {
   let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집';
 
-  function 함수() {
-    return 100;
+  function 제목바꾸기() {
+    // 수정할 데이터를 만든다.
+    // 이건 값 공유
+    // var newArray = 글제목;
+    // deep copy
+    var newArray = [...글제목];
+    newArray[0] = '여자 코드 추천'
+    글제목변경(newArray);
   }
-
-  // let style = { color : 'blue', fontSize : '30px' };
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      <button onClick={제목바꾸기}>버튼</button>
       <div className='list'>
         <h3>{글제목[0]} <span onClick={() => {따봉변경(따봉 + 1)}}>👍</span> {따봉} </h3>
         <p>2월 17일 발행</p>
