a73967f8417e687d76837b39008ea1739a2a37e6
Kiwon Hong <rusdlsla@gmail.com>
2020-12-08 19:54:51 +0900

    react 기초 4강 : 이벤트 리스너 (핸들러) 장착

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
@@ -6,6 +7,7 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코드 추천', '강남 우동 맛집', '선릉 국밥 맛집']);
 
+  let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집';
 
   function 함수() {
@@ -17,10 +19,10 @@ function App() {
   return (
     <div className="App">
       <div className="black-nav">
-        <div>개발 blog</div>
+        <div>개발 Blog</div>
       </div>
       <div className='list'>
-        <h3>{글제목[0]}</h3>
+        <h3>{글제목[0]} <span onClick={() => {따봉변경(따봉 + 1)}}>👍</span> {따봉} </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
