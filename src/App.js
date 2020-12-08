eaddac547abad6ab2dbdf047953e60e6929b7881
Kiwon Hong <rusdlsla@gmail.com>
2020-12-08 19:44:31 +0900

    react 기초 3강 : state 사용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState(['남자 코드 추천', '강남 우동 맛집', '선릉 국밥 맛집']);
 
   let posts = '강남 고기 맛집';
 
@@ -9,16 +12,28 @@ function App() {
     return 100;
   }
 
-  let style = { color : 'blue', fontSize : '30px' };
+  // let style = { color : 'blue', fontSize : '30px' };
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={ style }>개발 blog</div>
+        <div>개발 blog</div>
       </div>
-      <img src={ logo } />
-      <h4>{ posts }</h4>
-      {/*<h4>{ 함수() }</h4>*/}
+      <div className='list'>
+        <h3>{글제목[0]}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3>{글제목[1]}</h3>
+        <p>2월 18일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3>{글제목[2]}</h3>
+        <p>2월 19일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
