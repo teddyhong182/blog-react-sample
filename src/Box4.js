9db5ad579270a7683198f9e90c3bb526125af369
Kiwon Hong <rusdlsla@gmail.com>
2021-01-08 00:39:41 +0900

    동적 스타일 적용

--- src/Box4.js
+++ src/Box4.js
@@ -3,21 +3,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 const BoxCommon = styled.div`
+  width: ${props => (props.isBig ? 200 : 100)}px;
   height: 50px;
   background-color: #aaaaaa;
 `;
-const BoxBig = styled(BoxCommon)`
-  width: 200px;
-`;
-const BoxSmall = styled(BoxCommon)`
-  width: 100px;
-`;
 
 function Box({size}) {
-  if (size === 'big') {
-    return <BoxBig>큰 박스</BoxBig>;
-  } else {
-    return <BoxSmall>작은 박스</BoxSmall>;
-  }
+  const isBig = size === 'big';
+  const label = isBig ? '큰 박스' : '작은 박스';
+  return <BoxCommon isBig={isBig}>{label}</BoxCommon>;
 }
 export default Box;
